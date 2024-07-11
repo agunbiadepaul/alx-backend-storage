409951644e40755be190778fbe6093e279fe7079
agunbiadepaul <agunbiadepaul94@gmail.com>
2024-07-11 14:51:35 +0100

    Mysql advance

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- script that selects band_name, and lifespan column which is different
+
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
+    
